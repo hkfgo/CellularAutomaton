b0605b93502b4e188062f7c4ffc2fba42fa721d3
Stephen Chen <x.chen1016@gmail.com>
2013-11-16 17:37:58 -0500

    added automaton functions

--- src/AutoMata.java
+++ src/AutoMata.java
@@ -1,13 +1,25 @@
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 //Interface for automatonss
 public class AutoMata {
 	int stateCount=2;
-	static Hashtable rules;
-	public AutoMata(){
+	static Hashtable<String, String> rules;
+	boolean testCycle; //whether to test cycles
+	List<String> states;
+	String initialState;
+	public AutoMata(boolean cycle){
 		rules=new Hashtable<String,String>();
+		states= new ArrayList<String>();
+		testCycle=cycle;
 	}
 	
+	void setState(String state){
+		initialState=""+state;
+		System.out.println("State number 1: "+ initialState);
+		states.add(initialState);
+	}
 	void setRule(String... ruleStrings ){
 		for(String rule:ruleStrings){
 			rules.put(rule.split("->")[0], rule.split("->")[1]);
@@ -16,9 +28,16 @@ public class AutoMata {
 	}
 	
 	public void upDate(String state, int cycle){
+		state.replaceAll("\\s", "");
+		
 		String endState="";
-		if(cycle==1){
-			System.out.println("end state: "+ endState);
+		if(testCycle==true && states.indexOf(endState)!=-1){
+			System.out.println("Cycle found");
+			System.out.println("");
+		}
+		else if(cycle==1){
+			System.out.println("end state: "+ state);
+			System.out.println("All states: " + states.toString());
 		}
 		else{
 			for(int i=0;i<state.length();i++){
@@ -39,16 +58,12 @@ public class AutoMata {
 				}
 			}
 			System.out.println("State Number "+ stateCount+  ": "+ endState);
+			states.add(endState);
 			stateCount++;
 			upDate(endState,cycle-1);
 		}
 			
 	}
 	
-	public String newNumber(String neighbors){
-		String newNum="";
-		return newNum;
-		
-	}
 
 }
--- src/SolutionsMain.java
+++ src/SolutionsMain.java
@@ -3,13 +3,24 @@ public class SolutionsMain {
 	/**
 	to input data:
 	Automata am= new AutoMata();
-	am.setState("State as a string");
-	am.run(c) c is the number of cycles in integer 
+	am.setRule("10->1,"23->2" etc.) No input is needed if a rule sets a cell to 0 
+	am.upDate("11100000",15) initial state with no spaces between characters, number of cycles
 	 */
 	public static void main(String[] args){
-		AutoMata am= new AutoMata();
+		AutoMata am= new AutoMata(false);
+		/** Question number 1
 		am.setRule("10->1", "01->1");
 		System.out.println("State number 1: "+ "0110110110");
-		am.upDate("0110110110", 20);
+		am.setState("0110110110");
+		am.upDate(am.initialState, 50)
+		**/
+		/** 3a
+		am.setRule("10->1","01->1");
+		am.setState("01110");
+		am.upDate(am.initialState, 50);
+		**/
+		am.setRule("10->1","01->1");
+		am.setState("0111111");
+		am.upDate(am.initialState, 50);
 	}
 }
